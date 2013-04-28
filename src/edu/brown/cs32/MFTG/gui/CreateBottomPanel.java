cb546df85d5a52704ab58c2c07024536cf9093a0
Judah Schvimer <jschvime@cs.brown.edu>
2013-04-28 12:52:19 -0400

    create game

--- src/edu/brown/cs32/MFTG/gui/CreateBottomPanel.java
+++ src/edu/brown/cs32/MFTG/gui/CreateBottomPanel.java
@@ -1,5 +1,6 @@
 package edu.brown.cs32.MFTG.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -10,6 +11,8 @@ import java.awt.GridLayout;
 import java.awt.Point;
 import java.awt.RenderingHints;
 import java.awt.TextField;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -17,6 +20,7 @@ import java.util.regex.Pattern;
 
 import javax.imageio.ImageIO;
 import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -30,15 +34,17 @@ public class CreateBottomPanel extends JPanel {
 	private BufferedImage _whiteBack;
 	private final int BOTTOM_HEIGHT=Constants.FULL_HEIGHT*3/5;
 	private final int BOTTOM_WIDTH=Constants.FULL_WIDTH*15/16;
-	private JTextField _port, _timeBegin, _timeBetween, _numSets, _numGames;
-	private JLabel _portLabel,_winCondLabel, _timeBeginLabel, _timeBetweenLabel, _numSetsLabel, _numGamesLabel;
-	private JRadioButton _mostMoney, _mostSets, _lastMatch;
-	private ButtonGroup _winCond;
+	private JPanel _extraPanel, _fpPanel;
+	private JTextField _port, _timeBegin, _timeBetween, _numSets, _numGames, _freeParkingJackpot;
+	private JLabel _portLabel,_winCondLabel, _timeBeginLabel, _timeBetweenLabel, _numSetsLabel, _numGamesLabel, _gameFlowLabel, _extraLabel, _fpLabel;
+	private JRadioButton _mostMoney, _mostSets, _lastMatch, _chooseTogether, _rotateChoosing;
+	private JCheckBox _freeParking, _auctions, _doubleOnGo;
+	private ButtonGroup _winCond, _gameFlow;
 	public CreateBottomPanel() {
 		super();
 		try {
 			_whiteBack = Helper.resize(ImageIO.read(new File("images/WhiteBack.png")), BOTTOM_WIDTH, BOTTOM_HEIGHT);
-			this.setLayout(new GridLayout(0,1));
+			this.setLayout(new BorderLayout());
 			setBackground(Constants.CLEAR);
 			this.setSize(new Dimension(_whiteBack.getWidth(),_whiteBack.getHeight()));
 			this.setPreferredSize(new Dimension(_whiteBack.getWidth(),_whiteBack.getHeight()));
@@ -48,8 +54,11 @@ public class CreateBottomPanel extends JPanel {
 			
 			JPanel portPanel = new JPanel();
 			portPanel.setBackground(Color.WHITE);
+			Dimension portSize= new Dimension(BOTTOM_WIDTH,100);
+			portPanel.setSize(portSize);
+			portPanel.setPreferredSize(portSize);
 			_port = new JTextField(4);
-			_port.setDocument(new MyDocument());
+			_port.setDocument(new NumDocument(4));
 			_port.setSize(200,30);
 			_port.setFont(new Font("myFont",Font.PLAIN,40));
 
@@ -61,12 +70,14 @@ public class CreateBottomPanel extends JPanel {
 			
 			JPanel winCondPanel = new JPanel(new GridLayout(0,1));
 			winCondPanel.setAlignmentX(LEFT_ALIGNMENT);
-			winCondPanel.setBackground(Constants.CLEAR);
+			winCondPanel.setBackground(Color.WHITE);
 			_winCondLabel = new JLabel("Win Condition:");
 			_mostMoney=new JRadioButton("Most Cummulative Wealth");
 			_mostSets=new JRadioButton("Most Sets Won", true);
 			_lastMatch=new JRadioButton("Win Last Match");
-
+			_mostMoney.setBackground(Color.WHITE);
+			_mostSets.setBackground(Color.WHITE);
+			_lastMatch.setBackground(Color.WHITE);
 			
 			_winCond = new ButtonGroup();
 			_winCond.add(_mostSets);
@@ -78,45 +89,117 @@ public class CreateBottomPanel extends JPanel {
 			winCondPanel.add(_mostSets);
 			winCondPanel.add(_lastMatch);
 			
-			JPanel timePanel = new JPanel(new GridLayout(2,2));
+			JPanel timePanel = new JPanel(new GridLayout(0,1));
 			timePanel.setAlignmentX(LEFT_ALIGNMENT);
 			timePanel.setBackground(Color.WHITE);
 			_timeBeginLabel=new JLabel("Time At Beginning (min): ");
 			_timeBetweenLabel=new JLabel("Time At Beginning (min): ");
 			_timeBegin = new JTextField(4);
 			_timeBetween= new JTextField(4);
+			_timeBegin.setDocument(new NumDocument(1));
+			_timeBetween.setDocument(new NumDocument(1));
+			//_timeBegin.setFont(new Font("myFont",Font.PLAIN,20));
+			//_timeBetween.setFont(new Font("myFont",Font.PLAIN,20));
 			timePanel.add(_timeBeginLabel);
 			timePanel.add(_timeBegin);
 			timePanel.add(_timeBetweenLabel);
 			timePanel.add(_timeBetween);
 			
-			JPanel gameLengthPanel = new JPanel(new GridLayout(2,2));
+			JPanel gameLengthPanel = new JPanel(new GridLayout(0,1));
 			gameLengthPanel.setBackground(Color.WHITE);
 			_numSetsLabel=new JLabel("Number of Sets: ");
 			_numGamesLabel=new JLabel("Games per Set: ");
 			_numSets = new JTextField(4);
 			_numGames= new JTextField(4);
-			_numSets.setDocument(new MyDocument());
-			_numGames.setDocument(new MyDocument());
+			_numSets.setDocument(new NumDocument(3));
+			_numGames.setDocument(new NumDocument(5));
 			gameLengthPanel.add(_numSetsLabel);
 			gameLengthPanel.add(_numSets);
 			gameLengthPanel.add(_numGamesLabel);
 			gameLengthPanel.add(_numGames);
+			
+			JPanel gameFlowPanel = new JPanel(new GridLayout(0,1));
+			gameFlowPanel.setAlignmentX(LEFT_ALIGNMENT);
+			gameFlowPanel.setBackground(Color.WHITE);
+			_gameFlowLabel = new JLabel("Game Flow:");
+			_chooseTogether=new JRadioButton("Choose Heuristics Together", true);
+			_rotateChoosing=new JRadioButton("Rotate Choosing Heuristics");
+			_chooseTogether.setBackground(Color.WHITE);
+			_rotateChoosing.setBackground(Color.WHITE);
+			
+			_gameFlow = new ButtonGroup();
+			_gameFlow.add(_chooseTogether);
+			_gameFlow.add(_rotateChoosing);
+			
+			gameFlowPanel.add(_gameFlowLabel);
+			gameFlowPanel.add(_chooseTogether);
+			gameFlowPanel.add(_rotateChoosing);
+			
+			_extraPanel = new JPanel(new GridLayout(0,1));
+			_extraPanel.setBackground(Color.WHITE);
+			_extraLabel = new JLabel("In-Game Rules:");
+			_extraLabel.setBackground(Color.WHITE);
+			_fpPanel = new JPanel(new GridLayout(1,0));
+			_fpPanel.setBackground(Color.WHITE);
+			_freeParking = new JCheckBox("Free Parking", false);
+			_freeParking.addItemListener(new FreeParkingListener());
+			_fpLabel = new JLabel("Default Jackpot: ");
+			_freeParkingJackpot = new JTextField(4);
+			_freeParkingJackpot.setDocument(new NumDocument(3));
+			_fpPanel.add(_fpLabel);
+			_fpPanel.add(_freeParkingJackpot);
+			_auctions = new JCheckBox("Auctions", false);
+			_doubleOnGo = new JCheckBox("$400 landing on Go", false);
+			_extraPanel.add(_extraLabel);
+			_extraPanel.add(_auctions);
+			_extraPanel.add(_doubleOnGo);
+			_extraPanel.add(_freeParking);
+			_extraPanel.add(_fpPanel);
+			_fpPanel.setVisible(false);
 			
 			
 			
 			lowerPanel.add(winCondPanel);
 			lowerPanel.add(timePanel);
 			lowerPanel.add(gameLengthPanel);
+			lowerPanel.add(gameFlowPanel);
+			lowerPanel.add(_extraPanel);
 			
-			add(portPanel);
-			add(lowerPanel);
+			JPanel playerChoicePanel = new JPanel( new GridLayout(1,0));
+			playerChoicePanel.setBackground(Color.WHITE);
+			JPanel p1Panel = new JPanel(new GridLayout(0,1));
+			p1Panel.setBackground(Color.WHITE);
+			JLabel player1 = new JLabel("Player 1");
 			
-			//breaks things
-			_mostMoney.setBackground(Color.WHITE);
-			_mostSets.setBackground(Color.WHITE);
-			_lastMatch.setBackground(Color.WHITE);
+			p1Panel.add(player1);
 			
+			JPanel p2Panel = new JPanel(new GridLayout(0,1));
+			p2Panel.setBackground(Color.WHITE);
+			JLabel player2 = new JLabel("Player 2");
+			
+			p2Panel.add(player2);
+			
+			JPanel p3Panel = new JPanel(new GridLayout(0,1));
+			p3Panel.setBackground(Color.WHITE);
+			JLabel player3 = new JLabel("Player 3");
+			
+			p3Panel.add(player3);
+			
+			JPanel p4Panel = new JPanel(new GridLayout(0,1));
+			p4Panel.setBackground(Color.WHITE);
+			JLabel player4 = new JLabel("Player 4");
+			
+			p4Panel.add(player4);
+			
+			playerChoicePanel.add(p1Panel);
+			playerChoicePanel.add(p2Panel);
+			playerChoicePanel.add(p3Panel);
+			playerChoicePanel.add(p4Panel);
+			
+			add(portPanel, BorderLayout.NORTH);
+			add(lowerPanel, BorderLayout.CENTER);
+			add(playerChoicePanel, BorderLayout.SOUTH);
+
 
 		} catch (IOException e) {
 			System.out.println("ERROR: "+e.getMessage());
@@ -137,17 +220,22 @@ public class CreateBottomPanel extends JPanel {
 	 * @author jschvime
 	 *
 	 */
-	private class MyDocument extends PlainDocument{
+	private class NumDocument extends PlainDocument{
+		private int _maxLen;
+		public NumDocument(int maxLen) {
+			_maxLen=maxLen;
+		}
+		
 		@Override  
 		public void insertString(int offs, String str, AttributeSet a) throws BadLocationException  {  
 			if (!isInteger(str)) {
 				return;
 			}
-			if (this.getLength() < 4)  {  
+			if (this.getLength() < _maxLen)  {  
 				int newLength = this.getLength() + str.length();  
-				if (newLength> 4)  
+				if (newLength> _maxLen)  
 				{  
-					str = str.substring(0, 4 - this.getLength());  
+					str = str.substring(0, _maxLen - this.getLength());  
 				}  
 				super.insertString(offs, str, a);  
 			}  
@@ -166,6 +254,27 @@ public class CreateBottomPanel extends JPanel {
 			}
 			return true;
 		}
+	}
+	
+	/**
+	 * class that gets called when the checkbox changes
+	 * @author jschvime
+	 *
+	 */
+	private class FreeParkingListener implements ItemListener{
+
+		@Override
+		public void itemStateChanged(ItemEvent e) {
+			if(e.getStateChange()==ItemEvent.SELECTED) {
+				_fpPanel.setVisible(true);
+			}
+			else {
+				_fpPanel.setVisible(false);
+			}
+
+			
+		}
+		
 	}
 
 }
