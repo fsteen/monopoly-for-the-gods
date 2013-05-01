cd310a7c2bc4c825d507603324826e636620b7d7
Frances Steen <fsteen@cs.brown.edu>
2013-05-01 11:12:50 -0400

    added data processor test

--- src/edu/brown/cs32/MFTG/tournament/data/DataProcessorTests.java
+++ src/edu/brown/cs32/MFTG/tournament/data/DataProcessorTests.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Random;
 
 import org.junit.Test;
@@ -107,25 +108,14 @@ public class DataProcessorTests {
 		GameDataReport r = DataProcessor.aggregate(data, 50);
 		double totalGameWealth = 3000;
 		double actualGameWealth;
-//		for(int i = 0; i < r._timeStamps.size();i++){ //make sure the totalGameWealth is approx correct
-//			actualGameWealth = 0;
-//			for(PlayerWealthDataReport w : r._timeStamps.get(i).wealthData.values()){
-//				actualGameWealth += w.accTotalWealth;
-//				totalGameWealth += 200;
-//			}
-//			System.out.println(actualGameWealth + " " + totalGameWealth);
-////			assertTrue(actualGameWealth > totalGameWealth * .9 && actualGameWealth < totalGameWealth * 1.1);
-//		}
-//		
 		System.out.println(DataProcessor.aggregate(data, 50).toString());
 	}
 	
 	@Test
 	public void createRandomGameData(){
-		int numGames = 100;
-		int numTimestamps = 4;
+		int numGames = 1000;
+		int numTimestamps = 100;
 		int numPlayers = 4;
-		int numProperties = 5; 
 		
 		List<List<List<Double>>> totCash = new ArrayList<>();
 		List<List<Double>> cash; //for each timestamp for all players
@@ -135,27 +125,6 @@ public class DataProcessorTests {
 		List<List<Double>> wealth; //for each timestamp for all players
 		List<Double> wealthAtT;
 		
-//		List<List<List<Double>>> totHouses = new ArrayList<>();
-//		List<List<Double>> houses; //for each timestamp for all players
-//		List<Double> housesAtT;
-//		
-//		List<List<List<Double>>> totPRevW = new ArrayList<>();
-//		List<List<Double>> pRevW; //for each timestamp for all players
-//		List<Double> pRevWAtT;
-//		
-//		List<List<List<Double>>> totPRevWO = new ArrayList<>();
-//		List<List<Double>> pRevWO; //for each timestamp for all players
-//		List<Double> pRevWOAtT;
-//		
-//		List<List<List<Double>>> totTRevW = new ArrayList<>();
-//		List<List<Double>> tRevW; //for each timestamp for all players
-//		List<Double> tRevWAtT;
-//		
-//		List<List<List<Double>>> totTRevWO = new ArrayList<>();
-//		List<List<Double>> tRevWO; //for each timestamp for all players
-//		List<Double> tRevWOAtT;
-		
-//		List<PropertyData> tPDLsist;
 		List<PlayerWealthData> tPWDList;
 		
 		ArrayList<TimeStamp> timeStamps;
@@ -163,21 +132,10 @@ public class DataProcessorTests {
 		
 		List<GameData> gameData = new ArrayList<>();
 		GameData tGD;
-		
-//		int randPlayer;
-//		List<List<Integer>> randPlayers = new ArrayList<>(); //for each 
-//		List<Integer> tempRandPlayers;
-		
 		for(int gameNum = 0; gameNum < numGames; gameNum ++){
 			cash = new ArrayList<>();
 			wealth = new ArrayList<>();
-//			houses = new ArrayList<>();
-//			pRevW = new ArrayList<>();
-//			pRevWO = new ArrayList<>();
-//			tRevW = new ArrayList<>();
-//			tRevWO = new ArrayList<>();
 			timeStamps = new ArrayList<>();
-//			tempRandPlayers = new ArrayList<>();
 			for(int i = 0; i < numTimestamps; i++){
 				cashAtT = new ArrayList<>();
 				wealthAtT = new ArrayList<>();
@@ -190,32 +148,7 @@ public class DataProcessorTests {
 				}
 				wealth.add(wealthAtT);
 				cash.add(cashAtT);
-				
-//				housesAtT = new ArrayList<>();
-//				pRevWAtT = new ArrayList<>();
-//				pRevWOAtT = new ArrayList<>();
-//				tRevWAtT = new ArrayList<>();
-//				tRevWOAtT = new ArrayList<>();
-//				tPDList = new ArrayList<>();
-//				for(int k = 0; k < numProperties; k++){
-//					housesAtT.add(Math.random() * 5);
-//					pRevWAtT.add(Math.random() * 100);
-//					pRevWOAtT.add(Math.random() * 50);
-//					tRevWAtT.add(Math.random() * 200);
-//					tRevWOAtT.add(Math.random() * 75);
-//					randPlayer = (int)(Math.random() * numPlayers);
-//					tempRandPlayers.add(randPlayer);
-//					tPDList.add(new PropertyData("Property" + k,randPlayer,housesAtT.get(k),pRevWAtT.get(k),pRevWOAtT.get(k),tRevWAtT.get(k),tRevWOAtT.get(k),false));
-//				}
-//				pRevW.add(pRevWAtT);
-//				pRevWO.add(pRevWOAtT);
-//				tRevW.add(tRevWAtT);
-//				tRevWO.add(tRevWOAtT);
-//				houses.add(housesAtT);
-//				randPlayers.add(tempRandPlayers);
-				
 				tTS = new TimeStamp(i);
-//				tTS.setPropertyData(tPDList);
 				tTS.setWealthData(tPWDList);
 				timeStamps.add(tTS);
 			}
@@ -225,60 +158,37 @@ public class DataProcessorTests {
 			gameData.add(tGD);
 			totCash.add(cash);
 			totWealth.add(wealth);
-//			totHouses.add(houses);
-//			totPRevW.add(pRevW);
-//			totPRevWO.add(pRevWO);
-//			totTRevW.add(tRevW);
-//			totTRevWO.add(tRevWO);
 		}
 		
 		GameDataReport calculated = DataProcessor.aggregate(gameData, numTimestamps);
 		
 		GameDataReport actual = createGDR(totCash,totWealth);
 		
-		System.out.println(calculated);
-		System.out.println(actual);
+		assertTrue(calculated._timeStamps.size() == actual._timeStamps.size());
+		Map<Integer,PlayerWealthDataReport> c;
+		Map<Integer,PlayerWealthDataReport> a;
+		for(int i = 0; i < actual._timeStamps.size(); i++){
+			c = calculated._timeStamps.get(i).wealthData;
+			a = actual._timeStamps.get(i).wealthData;
+			
+			assertTrue(c.values().size() == a.values().size());
+			
+			PlayerWealthDataReport temp;
+			for(Entry<Integer, PlayerWealthDataReport> d : a.entrySet()){
+				temp = c.get(d.getKey());
+				assertTrue(temp != null);
+				assertTrue(temp.accCash == d.getValue().accCash);
+				assertTrue(temp.accTotalWealth == d.getValue().accTotalWealth);
+				assertTrue(temp.numDataPoints == d.getValue().numDataPoints);
+			}
+		}
 	}
 	
-	public GameDataReport createGDR(List<List<List<Double>>> cash, List<List<List<Double>>> wealth){ //, List<List<List<Double>>> houses, 
-//			List<List<List<Double>>> pRevW, List<List<List<Double>>> pRevWO, List<List<List<Double>>> tRevW,
-//			List<List<List<Double>>> tRevWO, List<List<List<Integer>>> randPlayers){
-	
-		List<List<Double>> aveCash = new ArrayList<>();
-		List<List<Double>> aveWealth = new ArrayList<>();
-//		List<List<Double>> aveHouses = new ArrayList<>();
-//		List<List<Double>> avePRevW = new ArrayList<>();
-//		List<List<Double>> avePRevWO = new ArrayList<>();
-//		List<List<Double>> aveTRevW = new ArrayList<>();
-//		List<List<Double>> aveTRevWO = new ArrayList<>();
-		
-		List<Double> cashAtT;
-		List<Double> wealthAtT;
-//		List<Double> housesAtT;
-//		List<Double> pRevWAtT;
-//		List<Double> pRevWOAtT;
-//		List<Double> tRevWAtT;
-//		List<Double> tRevWOAtT;
+	public GameDataReport createGDR(List<List<List<Double>>> cash, List<List<List<Double>>> wealth){ 
 		double cashSum;
 		double wealthSum;
-		
-//		Map<Integer, Double> pRevWSum;
-//		Map<Integer, Double> pRevWOSum;
-//		Map<Integer, Double> tRevWSum;
-//		Map<Integer, Double> tRevWOSum;
-//		double housesSum;
-//		
-//		double housesMax;
-//		Map<Integer, Double> pRevWMax;
-//		Map<Integer, Double> pRevWOMax;
-//		Map<Integer, Double> tRevWMax;
-//		Map<Integer, Double> tRevWOMax;
-//		
-//		PropertyDataReport p;
 		PlayerWealthDataReport w;
 		Map<Integer,PlayerWealthDataReport> wAtT;
-//		Map<String,PropertyDataReport> pAtT = new HashMap<>();
-//		Map<String,List<PropertyDataReport>> playerPAtT = new HashMap<>();
 		List<TimeStampReport> tStamps = new ArrayList<>();
 
 		for(int time = 0; time < cash.get(0).size(); time ++){
@@ -299,47 +209,7 @@ public class DataProcessorTests {
 				
 			tStamps.add(new TimeStampReport(time, wAtT));
 		}
-		///////////////////////////////////////////////////////////////
-//		
-//		for(int property = 0; property < houses.get(0).get(0).size(); property ++){
-//			
-//			 pRevWSum = new HashMap<>();
-//			 pRevWOSum = new HashMap<>();
-//			 tRevWSum = new HashMap<>();
-//			 tRevWOSum = new HashMap<>();
-//			 
-//			 housesSum = 0;
-//			for(int gameNum = 0; gameNum < cash.size(); gameNum ++){
-//				housesMax = 0;
-//				pRevWMax = new HashMap<>();
-//				pRevWOMax = new HashMap<>();
-//				 tRevWMax = new HashMap<>();
-//				 tRevWOMax = new HashMap<>();
-//
-//
-//				for(int time = 0; time < cash.get(0).size(); time ++){
-//					
-//					housesMax = Math.max(houses.get(gameNum).get(time).get(property),housesMax);
-//					pRevWMax.put(randPlayers.get(gameNum).get(time).get(property), )
-//					
-//					pRevWMax = Math.max(pRevW.get(gameNum).get(time).get(property),pRevWMax);
-//					pRevWOMax = Math.max(pRevWO.get(gameNum).get(time).get(property),pRevWOMax);
-//					tRevWMax = Math.max(tRevW.get(gameNum).get(time).get(property),tRevWMax);
-//					tRevWOMax = Math.max(tRevWO.get(gameNum).get(time).get(property),tRevWOMax);
-//				}
-//				housesSum += housesMax;
-//				pRevWSum += pRevWMax;
-//				pRevWOSum += pRevWOMax;
-//				tRevWSum += tRevWMax;
-//				tRevWOSum += tRevWOMax;
-//			}
-//			
-//			p = new PropertyDataReport("Property" + property, -1, housesSum/cash.size(), tRevWSum/cash.size(), tRevWOSum/cash.size(), 0.0, 0.0, cash.size());
-//			pAtT.put(p.propertyName, p);
-//		}
 		
 		return new GameDataReport(tStamps, 0, null, null);		
 	}
-	
-
 }
