d2ecb28efde87ca0e03ffb0933a91420a0d2715f
Judah Schvimer <jschvime@cs.brown.edu>
2013-05-08 14:26:11 -0400

    I'M AFRAID OF COMMITMENT

--- src/edu/brown/cs32/MFTG/mftg/Main.java
+++ src/edu/brown/cs32/MFTG/mftg/Main.java
@@ -1,5 +1,9 @@
 package edu.brown.cs32.MFTG.mftg;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 import edu.brown.cs32.MFTG.tournament.Client;
 import edu.brown.cs32.MFTG.tournament.HumanClient;
 
@@ -10,6 +14,12 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		/*try {
+			System.setErr(new PrintStream(new FileOutputStream("system_err.txt")));
+			System.setOut(new PrintStream(new FileOutputStream("system_out.txt")));
+		} catch (FileNotFoundException e) {
+			System.exit(1);
+		}*/
 		boolean music=false;
 		if(args.length>0&&args[0].equals("-music")){
 			music=true;
