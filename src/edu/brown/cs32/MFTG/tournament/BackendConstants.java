5d49d67fb1f48f5f0dbbf730d04ea14e30f21340
Damon Chizu Kawamoto <damon_kawamoto@brown.edu>
2013-05-08 23:35:11 -0400

    getting ready to pull

--- src/edu/brown/cs32/MFTG/tournament/BackendConstants.java
+++ src/edu/brown/cs32/MFTG/tournament/BackendConstants.java
@@ -5,7 +5,7 @@ public class BackendConstants {
 	public static final double CONFIRMATION_PERCENTAGE = .1; //this % of games will be checked for consistency
 	public static final int NUM_THREADS=10; //how many threads in which to run the games
 	public static final int DATA_PACKET_SIZE=100; //when the client is running games, update GUI after this many games
-	public static final int NUM_DATA_POINTS=1000; //combine game data into this many time stamps
+	public static final int NUM_DATA_POINTS=100; //combine game data into this many time stamps
 	public static final int MAX_NUM_TURNS=1000; //cut off a game after this many turns
 	public static final int DEFAULT_NUM_SETS=5; //the GUI starts out showing this many sets
 	public static final int DEFAULT_GAMES_PER_SET=1000; //the GUI starts out showing this many games
